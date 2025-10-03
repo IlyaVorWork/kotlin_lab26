ee0c7aa24a9d83f1c6b215f1fbc429b0dc0e4008
yydz <ilyavorwork2408@gmail.com>
2025-10-03 11:42:41 +0700

    initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,54 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.ilyavorontsov.lab26"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.ilyavorontsov.lab26"
+        minSdk = 35
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.10.0")
+    implementation("androidx.activity:activity:1.8.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    implementation("androidx.camera:camera-core:1.4.2")
+    implementation("androidx.camera:camera-camera2:1.4.2")
+    implementation("androidx.camera:camera-lifecycle:1.4.2")
+    implementation("androidx.camera:camera-view:1.4.2")
+    implementation("androidx.camera:camera-extensions:1.4.2")
+    implementation("com.google.firebase:firebase-crashlytics-buildtools:3.0.6")
+
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
---
+++ app/src/main/java/com/ilyavorontsov/lab26/MainActivity.kt
@@ -0,0 +1,136 @@
+package com.ilyavorontsov.lab26
+
+import android.content.pm.PackageManager
+import android.graphics.Color
+import android.os.Bundle
+import android.os.Environment
+import android.util.Log
+import android.view.View
+import android.widget.ImageButton
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.common.util.concurrent.ListenableFuture
+import java.io.File
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>
+    private lateinit var pvPreview: PreviewView
+
+    private lateinit var ibCapture: ImageButton
+
+    private lateinit var viewColorBox: View
+    private lateinit var tvColor: TextView
+
+    private fun initCamera() {
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+        cameraProviderFuture.addListener({
+            val cameraProvider = cameraProviderFuture.get()
+            val preview = Preview.Builder()
+                .build()
+            preview.surfaceProvider = pvPreview.getSurfaceProvider()
+
+            val cameraSelector = CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_FRONT)
+                .build()
+
+            val imageCapture = ImageCapture.Builder()
+                .setTargetRotation(pvPreview.display.rotation)
+                .build()
+
+            val imageAnalysis = ImageAnalysis.Builder()
+                .setOutputImageFormat(ImageAnalysis.OUTPUT_IMAGE_FORMAT_RGBA_8888)
+                .build()
+            imageAnalysis.setAnalyzer(ContextCompat.getMainExecutor(this)) { imageProxy ->
+                val buffer = imageProxy.planes[0].buffer
+                buffer.rewind()
+                val x = 320
+                val y = 240
+                val offset = (y * imageProxy.width + x) * 4
+                val r = buffer[offset + 0].toInt() and 0xff
+                val g = buffer[offset + 1].toInt() and 0xff
+                val b = buffer[offset + 2].toInt() and 0xff
+
+                viewColorBox.setBackgroundColor(Color.argb(255, r, g, b))
+                tvColor.text = "#${Integer.toHexString(r)}${Integer.toHexString(g)}${Integer.toHexString(b)}"
+
+                imageProxy.close()
+            }
+
+            cameraProvider.bindToLifecycle(
+                this,
+                cameraSelector,
+                imageCapture,
+                imageAnalysis,
+                preview
+            )
+
+            ibCapture = findViewById(R.id.ibCapture)
+            ibCapture.setOnClickListener {
+                val f = File(
+                    Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM),
+                    "photo.jpg"
+                )
+                val outputFileOptions = ImageCapture.OutputFileOptions.Builder(f).build()
+
+                imageCapture.takePicture(
+                    outputFileOptions,
+                    ContextCompat.getMainExecutor(this),
+                    object : ImageCapture.OnImageSavedCallback {
+                        override fun onError(error: ImageCaptureException) {
+                            Log.d("NEW PHOTO", "error")
+                        }
+
+                        override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                            Log.d("NEW PHOTO", "success")
+                        }
+                    })
+            }
+
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        if (isGranted)
+            initCamera()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        pvPreview = findViewById(R.id.pvPreview)
+        tvColor = findViewById(R.id.tvColor)
+        viewColorBox = findViewById(R.id.viewColorBox)
+
+        if (ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.CAMERA
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            requestPermissionLauncher.launch(android.Manifest.permission.CAMERA)
+        } else {
+            initCamera()
+        }
+    }
+}
